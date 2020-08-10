9b1d5a7a78ed3c49dacb850196d67efe34fe1fed
Vipin Kumar <resetpa7@gmail.com>
2020-08-10 13:17:59 +0530

    my First Commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,16 @@
+//javascript for responsive navbar strts
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+const navbar = document.getElementsByClassName('navbar')[0]
+
+toggleButton.addEventListener('click', ()=> {
+    navbarLinks.classList.toggle('active')    
+    navbar.style.height = 'auto';
+})
+
+// adjusting navbar height to auto when toggle button is pressed
+
+
+
+//javascript for responsive navbar ends
