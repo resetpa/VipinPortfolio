e765e73e9395e04a5532770d8707623b40ef3440
Vipin Kumar <resetpa7@gmail.com>
2020-08-25 18:10:02 +0530

    My first commit

---
+++ modules/contactschema.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+//dot env configuration
+const dotenv = require('dotenv').config();
+
+/*
+if (process.env.NODE_ENV !== 'production'){    
+  dotenv.config({path: './.env'});
+}
+*/
+//console.log(dotenv.parsed);
+mongoose.connect('mongodb+srv://process.env.MONGODB_DATA_USER_USERNAME:process.env.MONGODB_DATA_USER_PASSWORD@clustervipinportfolioo.s0tle.mongodb.net/vipinportfolio?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true//, useCreateIndex: true
+});
+var conn = mongoose.Collection;
+
+var contactPersonSchema = new mongoose.Schema({
+
+    Name: {
+        type: String,
+        required: true
+    },
+    Email: {
+        type: String,
+        required: true
+    },
+    PhoneNumber: {
+        type: Number,
+        required: true
+    }, 
+    Message: {
+        type: String,
+        required: true
+    },
+    Date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var contactPersonModel = mongoose.model('contactpersondetails', contactPersonSchema)
+module.exports = contactPersonModel;
+
